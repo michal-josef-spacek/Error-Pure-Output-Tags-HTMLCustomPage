bdcab4beff292fcd6abf8ff6bfca1e711672f509
Michal Špaček <tupinek@gmail.com>
2014-09-03 13:55:52 +0200

    Add second example to doc.

--- HTMLCustomPage.pm
+++ HTMLCustomPage.pm
@@ -93,7 +93,7 @@ Error::Pure::Output::Tags::HTMLCustomPage - Error::Pure HTML output helper.
 
 =back
 
-=head1 EXAMPLE
+=head1 EXAMPLE1
 
  # Pragmas.
  use strict;
@@ -123,7 +123,7 @@ Error::Pure::Output::Tags::HTMLCustomPage - Error::Pure HTML output helper.
          ['e', 'html'],
  ]);
 
- # Output:
+ # Output like:
  # Cache-Control: no-cache
  # Date: Wed, 03 Sep 2014 11:48:37 GMT
  # Content-Type: application/xhtml+xml
@@ -138,6 +138,43 @@ Error::Pure::Output::Tags::HTMLCustomPage - Error::Pure HTML output helper.
  #     Bar
  #   </div>
  # </html>
+
+=head1 EXAMPLE2
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Error::Pure::Output::Tags::HTMLCustomPage qw(err_pretty);
+ use Tags::Output::Raw;
+
+ # Tags object.
+ my $tags = Tags::Output::Raw->new(
+         'output_handler' => \*STDOUT,
+         'auto_flush' => 1,
+ );
+
+ # Error.
+ err_pretty($tags, 'utf-8', 'application/xhtml+xml', '1.0', [
+         ['b', 'html'],
+         ['b', 'head'],
+         ['b', 'title'],
+         ['d', 'Foo'],
+         ['e', 'title'],
+         ['e', 'head'],
+         ['b', 'div'],
+         ['d', 'Bar'],
+         ['e', 'div'],
+         ['e', 'html'],
+ ]);
+
+ # Output like:
+ # Cache-Control: no-cache
+ # Date: Wed, 03 Sep 2014 11:54:37 GMT
+ # Content-Type: application/xhtml+xml
+ # 
+ # <?xml version="1.0" encoding="utf-8" standalone="no"?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><html><head><title>Foo</title></head><div>Bar</div></html>
 
 =head1 DEPENDENCIES
 
--- examples/ex2.pl
+++ examples/ex2.pl
@@ -6,10 +6,10 @@ use warnings;
 
 # Modules.
 use Error::Pure::Output::Tags::HTMLCustomPage qw(err_pretty);
-use Tags::Output::Indent;
+use Tags::Output::Raw;
 
 # Tags object.
-my $tags = Tags::Output::Indent->new(
+my $tags = Tags::Output::Raw->new(
         'output_handler' => \*STDOUT,
         'auto_flush' => 1,
 );
@@ -30,16 +30,7 @@ err_pretty($tags, 'utf-8', 'application/xhtml+xml', '1.0', [
 
 # Output like:
 # Cache-Control: no-cache
-# Date: Wed, 03 Sep 2014 11:48:37 GMT
+# Date: Wed, 03 Sep 2014 11:54:37 GMT
 # Content-Type: application/xhtml+xml
 # 
-# <?xml version="1.0" encoding="utf-8" standalone="no"?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><html>
-#   <head>
-#     <title>
-#       Foo
-#     </title>
-#   </head>
-#   <div>
-#     Bar
-#   </div>
-# </html>
+# <?xml version="1.0" encoding="utf-8" standalone="no"?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><html><head><title>Foo</title></head><div>Bar</div></html>
